3944e9eb824d2e0b4e688ce8cd3d7373287c661b
PavelVesnin <pavel.vesnin.v@gmail.com>
2017-08-16 12:52:48 +0300

    added support for host and port, bump version to 1.0.7

--- src/drunken.config.example.php
+++ src/drunken.config.example.php
@@ -1,7 +1,11 @@
 <?php
 
 return [
-    'db' => 'drunken',
+    'database' => [
+        'host' => 'localhost',
+        'port' => 27017,
+        'name' => 'drunken',
+    ],
     'workers-dir' => __DIR__ . '/src/drunken_workers',
     'log_path' => __DIR__ . '/var/drunken.log',
     'hipchat' => [
